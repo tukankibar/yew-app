5090cb8e32a9af9b5b721d9a17b618254a05a85b
Tugkan Kibar <tugkan@orangebandits.com>
2022-11-04 08:39:54 +0100

    Add counter example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    // Underscore in Rust usually means something is _ignored_
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self {
+            value: 0,
+        }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
